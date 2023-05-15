d44f83ddd749bc76f590ed5b17f4516837e02a39
f22andbe <f22andbe@student.his.se>
2023-05-15 16:58:36 +0200

    Add Mountain class

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,60 @@
+package com.example.networking;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Mountain {
+    String name;
+    String location;
+    @SerializedName("size")
+    int height;
+
+
+
+    Mountain() {
+        name = "No name given";
+        location = "No location given";
+        height = -1; // signifies no height known
+
+    }
+
+    public Mountain(String name, String location, int height) {
+        this.name = name;
+        this.location = location;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Mountain{" +
+                "name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", height=" + height +
+                '}';
+    }
+}
